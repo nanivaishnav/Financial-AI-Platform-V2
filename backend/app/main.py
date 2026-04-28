9ea4cc5187ef5958ab2e962f70566d76cb44ff4f
Nani Vaishnav <nanivaishnav45@gmail.com>
2026-04-28 12:34:29 +0530

    updated backend main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI
-from app.core.database import Base, engine
-from app.api.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
-from app.models.user import User
+app = FastAPI()
 
-Base.metadata.create_all(bind=engine)
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
 
-app = FastAPI(
-    title="Financial AI Platform"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-
-app.include_router(auth_router)
 
 @app.get("/")
 def root():
