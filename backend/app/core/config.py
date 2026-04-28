aa247064fc5efc071876c95ced1ce9e0d19cbddb
Nani Vaishnav <nanivaishnav45@gmail.com>
2026-04-28 11:39:56 +0530

    updated config and database

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,10 +1,10 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+SECRET_KEY = os.getenv("SECRET_KEY", "my_super_secret_key")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(
-    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
 )
