adc94e632a246420d71d098b85754df903d93794
Nani Vaishnav <nanivaishnav45@gmail.com>
2026-05-07 10:45:07 +0530

    updated chat.jsx with questions and answers from data

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,3 +1,51 @@
+import { useState } from "react";
+import API from "../api";
+
 export default function Chat() {
-  return <h1>Chat Page</h1>;
+
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const handleAsk = async () => {
+
+    try {
+
+      const res = await API.post("/chat", {
+        question
+      });
+
+      setAnswer(res.data.answer);
+
+    } catch {
+      alert("Chat failed");
+    }
+  };
+
+  return (
+    <div className="content">
+
+      <h2>Chat With Financial Report</h2>
+
+      <textarea
+        rows="4"
+        placeholder="Ask financial questions..."
+        onChange={(e) => setQuestion(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={handleAsk}>
+        Ask AI
+      </button>
+
+      {answer && (
+        <div style={{ marginTop: "30px" }}>
+          <h3>AI Answer</h3>
+
+          <pre>{answer}</pre>
+        </div>
+      )}
+
+    </div>
+  );
 }
