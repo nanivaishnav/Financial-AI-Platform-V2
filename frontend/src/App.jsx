df93011bbc2ab2aac7e1647c6fb1aeb8fc35db8e
Nani Vaishnav <nanivaishnav45@gmail.com>
2026-05-07 10:46:42 +0530

    imported page/chat to app.jsx and route the path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Chat from "./pages/Chat";
+
+<Route path="/chat" element={<Chat />} />
 
 function App() {
   const [count, setCount] = useState(0)
